049f8595eca10614d3edb4b1a4c1425d6d08ba75
barnajit <35171460+barnajit@users.noreply.github.com>
2018-03-01 14:23:22 +0800

    Update test_inception.py

--- test_inception.py
+++ test_inception.py
@@ -5,19 +5,12 @@ from keras.models import Sequential
 from keras.layers import Dropout, Flatten, Dense
 from keras import applications
 from keras.models import Model
-
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras import backend as K
 from keras import regularizers
-
 from keras.models import load_model
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
-
 from keras.preprocessing.image import img_to_array, load_img
-import numpy as np
 import tensorflow as tf
 
 
@@ -25,7 +18,6 @@ import tensorflow as tf
 img_width, img_height = 200, 200
 
 top_model_weights_path = r'bottleneck_fc_model_randlayermy.h5'
-#train_data_dir = r'E:\test\train2mirror'
 train_data_dir = r'D:\aesta\tester'
 
 nb_train_samples = 7834
@@ -34,16 +26,9 @@ epochs = 15
 batch_size = 20
 batch_size1=20
 
-#base_model = applications.InceptionV3(include_top=False, weights='imagenet')
-
-#base_model.summary()
-
-
 
 def save_bottlebeck_features():
     datagen = ImageDataGenerator(rescale=1. / 255)
-
-    # build the VGG16 network
    
     base_model = applications.InceptionV3(include_top=False, weights='imagenet')
     model = Model(inputs=base_model.input, outputs=base_model.get_layer(name='mixed2').output)
@@ -58,14 +43,7 @@ def save_bottlebeck_features():
 
 
     np.save(open('timepass.npy', 'wb'),bottleneck_features_train)
-
     train_data = np.load(open('timepass.npy', 'rb'))
-
-
-    #train_data = bottleneck_features_train
-
- 
-
 
     print(np.shape(train_data))
 
@@ -74,7 +52,7 @@ def save_bottlebeck_features():
     model = Sequential()
     model.add(Conv2D(32, (3, 3), input_shape=train_data.shape[1:]))
     model.add(Activation('relu'))
-    #model.add(MaxPooling2D(pool_size=(2, 2)))
+  
 
     model.add(Conv2D(32, (3, 3), ))
     model.add(Activation('relu'))
@@ -82,7 +60,7 @@ def save_bottlebeck_features():
 
     model.add(Conv2D(64, (3, 3),kernel_regularizer=regularizers.l1(0.01)))
     model.add(Activation('relu'))
-    #model.add(MaxPooling2D(pool_size=(2, 2)))
+    
 
     model.add(Flatten())
     model.add(Dense(64))
@@ -102,13 +80,10 @@ def save_bottlebeck_features():
         x = np.expand_dims(x, axis=0)
         preds = model.predict_classes(x)
         probs = model.predict_proba(x)
-        #if (probs<0.3) or (preds==1) :
         print("%s   %s" % (probs, preds))
         sumo=sumo+1
           
     print(sumo)
-        
-
     print('done')
 
 
